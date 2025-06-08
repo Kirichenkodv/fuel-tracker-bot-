727ab4df6abf779f0a63b613e234acbe163cd0eb
kirichenko dmitriy <kirichenko.dmitriy1977@gmail.com>
2025-06-08 15:23:47 +1000

    feat(infra): finalize database setup - Update docker-compose.yaml with db service - Configure alembic (env.py, script.py.mako) - Generate initial migration - Refactor config.py settings loader

--- src/core/config.py
+++ src/core/config.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 from typing import AsyncGenerator
 
 from dotenv import load_dotenv
@@ -9,40 +9,25 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 
-class DatabaseSettings(BaseSettings):
-    """Настройки подключения к PostgreSQL."""
-
-    POSTGRES_USER: str = os.getenv('POSTGRES_USER', 'postgres')
-    POSTGRES_PASSWORD: str = os.getenv('POSTGRES_PASSWORD', '')
-    POSTGRES_DB: str = os.getenv('POSTGRES_DB', 'postgres')
-    POSTGRES_HOST: str = os.getenv('POSTGRES_HOST', 'localhost')
-    POSTGRES_PORT: str = os.getenv('POSTGRES_PORT', '5432')
-
-    class Config:
-        """Конфигурация Pydantic для обработки настроек."""
-
-        extra = 'ignore'  # Игнорировать дополнительные переменные
-
-
-class BotSettings(BaseSettings):
-    """Настройки Telegram бота."""
-
-    TOKEN: str = os.getenv('TOKEN', '')
-    """Токен бота, обязательный параметр"""
-
-    class Config:
-        """Конфигурация Pydantic для обработки настроек."""
-
-        extra = 'ignore'
-
-
-class Settings(DatabaseSettings, BotSettings):
+class Settings(BaseSettings):
     """Общие настройки приложения."""
 
+    # Настройки DB
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str = 'localhost'
+    POSTGRES_PORT: str = '5432'
+
+    # Настройки бота
+    TOKEN: str
+
     class Config:
         """Конфигурация Pydantic для обработки настроек."""
 
-        extra = 'ignore'
+        # Указываем путь к .env относительно расположения config.py
+        env_file = Path(__file__).parent.parent.parent / 'infra' / '.env'
+        env_file_encoding = 'utf-8'
 
 
 settings = Settings()
@@ -52,6 +37,7 @@ SQLALCHEMY_DATABASE_URL = (
     f'postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@'
     f'{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}'
 )
+
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(
     engine,
